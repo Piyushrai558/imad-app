9b1311007280f426bb9437a2b74f169ad366e351
Piyushrai558 <31138966+Piyushrai558@users.noreply.github.com>
2018-03-30 16:23:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,12 @@ var element=document.getElementById('main-text')
 element.innerHTML='piyush rai'
 //image property
 var img= document.getElementById('insta')
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+5;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onclick = function(){
-  img.style.marginLeft='500px'; 
+  var Interval=setInterval(moveRight,50);
 };
 
