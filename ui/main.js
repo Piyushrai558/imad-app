e7efb76cc618480de4083bacda761f20162a7c4d
Piyushrai558 <31138966+Piyushrai558@users.noreply.github.com>
2018-03-30 16:12:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,5 +2,9 @@ console.log('Loaded!');
 //change the text
 var element=document.getElementById('main-text')
 element.innerHTML='piyush rai'
-
+//image property
+var img= document.getElementById('insta')
+img.onclick = function(){
+  img.style.marginup='500px'; 
+};
 
