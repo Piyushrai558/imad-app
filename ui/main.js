a80a085d7c80da48f9f7cfc860fe1da27f989a0b
Piyushrai558 <31138966+Piyushrai558@users.noreply.github.com>
2018-03-30 17:11:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,13 @@
-console.log('Loaded!');
-//change the text
-var element=document.getElementById('main-text')
-element.innerHTML='piyush rai'
-//image property
-var img= document.getElementById('insta')
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+5;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick = function(){
-  var Interval=setInterval(moveRight,50);
+//counter code
+var button= document.getElementById("counter");
+button.onclick= function (){
+    //make request at the counter endpoint
+    
+    //capture the respond and store in the variable 
+    
+    //render the variable in correct spam
+    counter = counter +1;
+    var span= document.getElementById('count')
+    span.innerHTMl=counter.toString();
 };
 
